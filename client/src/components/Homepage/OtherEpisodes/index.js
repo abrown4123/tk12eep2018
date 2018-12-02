1855d088fe7edc2044f6a7b4397cbee0b75d954a
Anthony Brown <abrown4123@gmail.com>
2018-12-01 20:15:15 -0600

    Add OtherEpisode components with dummy data

--- client/src/components/Homepage/OtherEpisodes/index.js
+++ client/src/components/Homepage/OtherEpisodes/index.js
@@ -1,12 +1,28 @@
 import React from "react";
+let episodes = ['a','b'];
 
 const OtherEpisodes = () => {
 
+    const episodeTiles = episodes.map((episode, i) => {
+        return (
+            <div key={i} class="episode-img-container">
+                <img src="http://policetax.com/images/galleries/style/jquery/full-page/examples/imgs/bg5.jpg" alt="Episode cover art" />
+                <div class="episode-info-container">
+                    <div class="episode-info">
+                        <h3 class="episode-title"><a href="#">Recent Episode Title</a></h3>
+                        <div class="episode-meta">Season 3 &middot; Episode 16</div>
+                    </div>
+                </div>
+            </div>
+        )
+    });
+
 
     return (
-        <section className="episodes-other">
-            <h2 className="section-title">Also check out these other episodes:</h2>
+        <React.Fragment >
             { episodeTiles }
-        </section>
+        </ React.Fragment>
     );
 }
+
+export default OtherEpisodes;
--- client/src/components/Homepage/index.js
+++ client/src/components/Homepage/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import EpisodePrimary from './EpisodePrimary';
+import OtherEpisodes from './OtherEpisodes';
+
+//TODO: The Other episodes component needs to be passed props once the backend is ready since the episode 
+//blocks are the same
 
 const Homepage = () => {
     return (
         <main className='episode-container'>
             <EpisodePrimary />
+            <section className="episodes-other">
+                <h2 className="section-title">Also check out these other episodes:</h2>
+                <div className='episodes-recent'>
+                    <OtherEpisodes />
+                </div>
+                <div className="episodes-related">
+                    <OtherEpisodes />
+                </div>
+                <div class="more-episodes">
+                    <a href="#">More Episodes <i class="fas fa-fast-forward"></i></a>
+                </div>
+            </section>
         </main>
     );
 }
